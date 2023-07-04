4f990f0fcadc73cc4023efc807182aa4fab0b2f8
ChadChr1st <94069594+ChadChr1st@users.noreply.github.com>
2023-07-04 03:02:21 +0000

    progress to report out

--- src/IOCtrl.java
+++ src/IOCtrl.java
@@ -49,16 +49,28 @@ String Filepath ;
 ArrayList<String> lines;
 User tempUser;
 
-private String lineBuilder(User buildData, int index){
+private String lineBuilder(User buildData, int index, int sml){
 //todo: 
-//-enable functionality to allow for cvs to have blank spots where no values exist such as in the case of more user has more expenses than 
+//-enable functionality to allow for cvs to have blank spots where no values exist 
+// such as in the case of more user has more expenses than incomes or vice versa
 String returnable;
 String blankline = ",,,";
 
-returnable+=
 
 
+if (buildData.incomes.size() == sml){
+    Income incIemp = buildData.incomes.get(index);
+    Expense exTemp = buildData.Spending.get(index);
 
+    returnable = incIemp.source + ","
+                +incIemp.amount + ","
+                +incIemp.Month + ","
+                +exTemp.amount + "," 
+                +exTemp.source + ","
+                +exTemp.yearlyfrequency;
+}else if(buildData.Spending.size() == sml){
+
+}
 
 
 return "dodo";
@@ -70,6 +82,7 @@ public boolean makeReport(User writeData, String filename){
     boolean success = false;
     String line = "test";
     int repeats;
+    int smaller;
 
     ArrayList<String> lines = new ArrayList<String>();
     
@@ -79,20 +92,23 @@ public boolean makeReport(User writeData, String filename){
     //determine how many rows will be in final report by the size of the larger of the incomes or spending lists
     if (writeData.incomes.size()> writeData.Spending.size()){
         repeats = writeData.incomes.size();
+        smaller = writeData.Spending.size();
     }else{
         repeats = writeData.Spending.size();
+        smaller = writeData.incomes.size();
     }
 
-    //
+    //build lines with lineBuilder 
     for (int j = 0; j < repeats; j++){
-        lines.add(lineBuilder(writeData, j));
+        lines.add(lineBuilder(writeData, j, smaller));
     }
 
+    //meat and potatoes, using printwriter to print lines built above to a file with header of .csv
     try {
-        PrintWriter output = new PrintWriter(filename + ".txt");
+        PrintWriter output = new PrintWriter(filename + ".csv");
         
 
-
+        output.print(headers)
         for(int i = 0; i<1;i++){
         output.println(line);
         }
