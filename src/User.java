ab9f27a77a6fbe302603c1faca6aa0ba3e6a26ae
ChadChr1st <94069594+ChadChr1st@users.noreply.github.com>
2023-07-05 11:42:30 +0000

    bug fixxes and user class constructor implementing

--- src/User.java
+++ src/User.java
@@ -1,11 +1,9 @@
 import java.util.ArrayList;
 
 public class User {
-	private ArrayList <Currency>currencyRates;
-
-	private ArrayList <Income>Income;  // user income sources that user can record or view or search by type or month 
-	private ArrayList <Expense>Spending; //user's expenses 
-
+	//private ArrayList <Currency>currencyRates;
+	public ArrayList <Income>incomes;  // user income sources that user can record or view or search by type or month 
+	public ArrayList <Expense>Spending; //user's expenses 
 	String username;
 	String pwd;
 	//current total income - total 
@@ -13,5 +11,17 @@ public class User {
 	// possible monthly savings, calculated using monthly income (most recent) assuming the data we have is for one year, and monthly and biweekly expenses, here you can assume yearly expenses that are recorded have already been paid. 
 	double monthlysavings;	
 	//should add constructor(s)
-	User(String username,String password){}
+	User(String uname,String password){
+		incomes = new ArrayList<Income>();
+		Spending = new ArrayList<Expense>();
+		username = uname;
+		pwd = password;
+
+	}
+	User(){
+		incomes = new ArrayList<Income>();
+		Spending = new ArrayList<Expense>();
+		username = "unset";
+		pwd = "unset";
+	}
 }
